23f3a126b80d44938108cc21eb348c950f795375
sfmig <33267254+sfmig@users.noreply.github.com>
2023-09-20 16:59:43 +0100

    rename init

--- brainglobe_workflows/__init__.py
+++ brainglobe_workflows/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("brainglobe-scripts")
+    __version__ = version("brainglobe-workflows")
 except PackageNotFoundError:
     # package is not installed
     pass
