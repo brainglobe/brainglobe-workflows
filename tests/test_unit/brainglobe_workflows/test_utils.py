ea731dcc732764c28cf47bf9734fd8f91bba67cc
sfmig <33267254+sfmig@users.noreply.github.com>
2023-12-07 11:09:57 +0000

    remove condition for logger one handler only

--- tests/test_unit/brainglobe_workflows/test_utils.py
+++ tests/test_unit/brainglobe_workflows/test_utils.py
@@ -9,5 +9,4 @@ def test_setup_logger(custom_logger_name):
     assert logger.level == logging.DEBUG
     assert logger.name == custom_logger_name
     assert logger.hasHandlers()
-    assert len(logger.handlers) == 1
     assert logger.handlers[0].name == "console_handler"
