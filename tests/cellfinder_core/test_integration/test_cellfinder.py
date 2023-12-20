91284b95feafc7142d5879d67e2f60b63815da19
sfmig <33267254+sfmig@users.noreply.github.com>
2023-12-20 20:31:15 +0000

    all tests passing with default option

--- tests/cellfinder_core/test_integration/test_cellfinder.py
+++ tests/cellfinder_core/test_integration/test_cellfinder.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 from pathlib import Path
 from brainglobe_workflows.cellfinder_core.cellfinder_core import main
+import pytest
+from typing import Optional
 
 
 def test_main():
@@ -11,6 +13,44 @@ def test_main():
 
     # run main
     cfg = main()
+
+    # check output files exist
+    assert Path(cfg._detected_cells_path).is_file()
+
+
+@pytest.mark.parametrize(
+    "input_config",
+    [
+        None,
+        # "input_config_fetch_GIN",
+        # "input_config_fetch_local",
+    ],
+)
+def test_main_w_inputs(
+    input_config: Optional[str],
+    request: pytest.FixtureRequest,
+):
+    """Test main function for setting up and running cellfinder workflow
+    with inputs
+
+    Parameters
+    ----------
+    input_config : Optional[str]
+        Path to input config json file
+    monkeypatch : pytest.MonkeyPatch
+        Pytest fixture to use monkeypatching utils
+    tmp_path : Path
+        Pytest fixture providing a temporary path for each test
+    request : pytest.FixtureRequest
+        Pytest fixture to enable requesting fixtures by name
+    """
+    # monkeypatch to change current directory to
+    # pytest temporary directory
+    # (cellfinder cache directory is created in cwd)
+    # monkeypatch.chdir(tmp_path)
+
+    # run main
+    cfg = main(str(request.getfixturevalue(input_config)))
 
     # check output files exist
     assert Path(cfg._detected_cells_path).is_file()
@@ -44,8 +84,19 @@ def test_script():
 
 def test_entry_point():
     """Test running the cellfinder workflow via the predefined entry point
-    without inputs
+    with no inputs
+
+    Parameters
+    ----------
+    monkeypatch : pytest.MonkeyPatch
+        Pytest fixture to use monkeypatching utils
+    tmp_path : Path
+        Pytest fixture providing a temporary path for each test
     """
+    # monkeypatch to change current directory to
+    # pytest temporary directory
+    # (cellfinder cache directory is created in cwd)
+    # monkeypatch.chdir(tmp_path)
 
     # define CLI input
     subprocess_input = ["cellfinder-workflow"]
