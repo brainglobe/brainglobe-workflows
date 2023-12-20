574733ccf6ecf283a24df17b68f733f313160ef0
sfmig <33267254+sfmig@users.noreply.github.com>
2023-12-20 19:36:31 +0000

    mark GIN download data test as slow

--- tests/cellfinder_core/conftest.py
+++ tests/cellfinder_core/conftest.py
@@ -7,6 +7,21 @@ import pooch
 import pytest
 
 
+@pytest.fixture()
+def default_input_config_cellfinder() -> Path:
+    """Return path to default input config for cellfinder workflow
+
+    Returns
+    -------
+    Path
+        Path to default input config
+
+    """
+    from brainglobe_workflows.utils import DEFAULT_JSON_CONFIG_PATH_CELLFINDER
+
+    return DEFAULT_JSON_CONFIG_PATH_CELLFINDER
+
+
 @pytest.fixture(autouse=True)
 def mock_home_directory(monkeypatch: pytest.MonkeyPatch):
     # define mock home path
@@ -53,44 +68,82 @@ def cellfinder_GIN_data() -> dict:
 
 
 @pytest.fixture()
-def config_GIN(cellfinder_GIN_data):
+def config_GIN(cellfinder_GIN_data, default_input_config_cellfinder):
+    """
+    Return a config pointing to the location where GIN would be by default
+    """
+
+    from brainglobe_workflows.cellfinder_core.cellfinder import (
+        CellfinderConfig,
+    )
+
+    # download data to default location for GIN
+    pooch.retrieve(
+        url=cellfinder_GIN_data["url"],
+        known_hash=cellfinder_GIN_data["hash"],
+        path=Path.home()
+        / ".brainglobe"
+        / "workflows"
+        / "cellfinder_core",  # path to download zip to
+        progressbar=True,
+        processor=pooch.Unzip(extract_dir="cellfinder_test_data"),
+    )
+
+    # read default config as dict
+    with open(default_input_config_cellfinder) as cfg:
+        config_dict = json.load(cfg)
+
+    # modify / ensure
+    # - add url
+    # - add data hash
+    # - add input_data_dir
+    config_dict["data_url"] = cellfinder_GIN_data["url"]
+    config_dict["data_hash"] = cellfinder_GIN_data["hash"]
+    if "input_data_dir" in config_dict.keys():
+        del config_dict["input_data_dir"]
+
+    # instantiate object
+    config = CellfinderConfig(**config_dict)
+
+    return config
+
+
+@pytest.fixture()
+def config_force_GIN(config_GIN, tmp_path):
     """
     Return a config pointing to the location where GIN would be by default
     """
     from brainglobe_workflows.cellfinder_core.cellfinder import (
-        read_cellfinder_config,
-    )
-    from brainglobe_workflows.utils import DEFAULT_JSON_CONFIG_PATH_CELLFINDER
-
-    # fetch data from GIN and download locally
-    # if it exists, pooch doesnt download again
-    pooch.retrieve(
-        url=cellfinder_GIN_data["url"],
-        known_hash=cellfinder_GIN_data["hash"],
-        path=Path.home(),  # path to download zip to
-        progressbar=True,
-        processor=pooch.Unzip(
-            extract_dir="cellfinder_test_data"
-            # path to unzipped dir, *relative*  to 'path'
-        ),
+        CellfinderConfig,
     )
 
-    return read_cellfinder_config(DEFAULT_JSON_CONFIG_PATH_CELLFINDER)
-    # read_cellfinder_config(input_configs_dir / "input_data_GIN.json")
+    # ensure neither signal or background dir exist!
+    config_dict = config_GIN.__dict__
+    config_dict["input_data_dir"] = tmp_path
+    # since the input_data_dir does not exist, it will
+    # download the GIN data there
+
+    # instantiate object
+    config = CellfinderConfig(**config_dict)
+
+    # ensure neither signal or background dir exist, to force GIN case
+    assert not Path(config._signal_dir_path).exists()
+    assert not Path(config._background_dir_path).exists()
+
+    return config
 
 
 @pytest.fixture()
-def config_local(cellfinder_GIN_data):
+def config_local(cellfinder_GIN_data, default_input_config_cellfinder):
     """ """
 
     from brainglobe_workflows.cellfinder_core.cellfinder import (
         CellfinderConfig,
     )
-    from brainglobe_workflows.utils import DEFAULT_JSON_CONFIG_PATH_CELLFINDER
 
     # read default config as dict
     # as dict because some paths are computed derived from input_data_dir
-    with open(DEFAULT_JSON_CONFIG_PATH_CELLFINDER) as cfg:
+    with open(default_input_config_cellfinder) as cfg:
         config_dict = json.load(cfg)
 
     # modify location of data?
@@ -99,7 +152,7 @@ def config_local(cellfinder_GIN_data):
     # - add input_data_dir
     config_dict["data_url"] = None
     config_dict["data_hash"] = None
-    config_dict["input_data_dir"] = Path.home() / "local_data"
+    config_dict["input_data_dir"] = Path.home() / "local_cellfinder_data"
 
     # instantiate object
     config = CellfinderConfig(**config_dict)
