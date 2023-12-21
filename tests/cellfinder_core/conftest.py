242668da77a8a35c0293a4cf63f129d25ef6d50b
sfmig <33267254+sfmig@users.noreply.github.com>
2023-12-21 11:11:29 +0000

    cosmetic changes to fixtures

--- tests/cellfinder_core/conftest.py
+++ tests/cellfinder_core/conftest.py
@@ -4,29 +4,14 @@ from pathlib import Path
 
 import pytest
 
-# @pytest.fixture()
-# def default_input_config_cellfinder() -> Path:  # do I need this?
-#     """Return path to default input config for cellfinder workflow
-
-#     Returns
-#     -------
-#     Path
-#         Path to default input config
-
-#     """
-#     from brainglobe_workflows.utils import
-# DEFAULT_JSON_CONFIG_PATH_CELLFINDER
-
-#     return DEFAULT_JSON_CONFIG_PATH_CELLFINDER
-
 
 @pytest.fixture(autouse=True)
 def mock_home_directory(monkeypatch: pytest.MonkeyPatch):
     # define mock home path
     home_path = Path.home()  # actual home path
-    mock_home_path = home_path / ".brainglobe-tests"  # tmp_path  #
+    mock_home_path = home_path / ".brainglobe-tests"
 
-    # create dir if it doesn't exist
+    # create directory if it doesn't exist
     if not mock_home_path.exists():
         mock_home_path.mkdir()
 
