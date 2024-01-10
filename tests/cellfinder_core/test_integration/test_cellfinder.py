a3e53709f3fda4610f72fa141bc5bf468dc8a36c
sfmig <33267254+sfmig@users.noreply.github.com>
2024-01-10 15:28:02 +0000

    remove tests for running script (monkeypatched home function not passed to subprocess, and all subsequent steps already tested)

--- tests/cellfinder_core/test_integration/test_cellfinder.py
+++ tests/cellfinder_core/test_integration/test_cellfinder.py
@@ -1,9 +1,7 @@
 import subprocess
-import sys
 from pathlib import Path
 import pytest
 from typing import Optional
-
 
 def test_main():
     """
@@ -50,80 +48,6 @@ def test_main_w_inputs(
 
     # check output files exist
     assert Path(cfg._detected_cells_path).is_file()
-
-
-@pytest.mark.skip()
-def test_script():
-    """
-    Test running the cellfinder worklfow from the command line
-    with no inputs
-    """
-    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
-        __file__ as script_path,
-    )
-
-    # define CLI input
-    subprocess_input = [
-        sys.executable,
-        str(script_path),
-    ]
-
-    # run workflow
-    # Path.home() is not monkeypatched :(
-    subprocess_output = subprocess.run(
-        subprocess_input,
-    )
-
-    # check returncode
-    assert subprocess_output.returncode == 0
-
-
-@pytest.mark.skip()
-@pytest.mark.parametrize(
-    "input_config",
-    [
-        "config_local_json",
-        "config_GIN_json",
-    ],
-)
-def test_script_w_inputs(
-    input_config: Optional[str],
-    request: pytest.FixtureRequest,
-):
-    """
-    Test running the cellfinder worklfow from the command line with inputs
-
-    Parameters
-    ----------
-    input_config : Optional[str]
-        Path to input config json file
-    request : pytest.FixtureRequest
-        Pytest fixture to enable requesting fixtures by name
-    """
-
-    # path to script
-    script_path = (
-        Path(__file__).resolve().parents[3]
-        / "brainglobe_workflows"
-        / "cellfinder_core"
-        / "cellfinder.py"
-    )
-
-    # define CLI input
-    subprocess_input = [
-        sys.executable,
-        str(script_path),
-    ]
-    subprocess_input.append("--config")
-    subprocess_input.append(str(request.getfixturevalue(input_config)))
-
-    # run workflow
-    subprocess_output = subprocess.run(
-        subprocess_input,
-    )
-
-    # check returncode
-    assert subprocess_output.returncode == 0
 
 
 @pytest.mark.skip()
