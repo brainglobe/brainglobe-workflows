017f005333d3c234eda15a0eed71ef8e02e2499a
sfmig <33267254+sfmig@users.noreply.github.com>
2024-01-10 18:31:56 +0000

    Cellfinder config + path change (#64)
    * pass all parameters to cellfinder_run
    
    * changed required, optional and internal fields. read_config passes. add method to config class (WIP)
    
    * add signal and background data from local
    
    * add missing data config fixtures
    
    * mark GIN download data test as slow
    
    * refactor config class methods and remove setup_workflow
    
    * tests pass with default config
    
    * remove unused fixtures
    
    * update entry point
    
    * change cellfinder-core to cellfinder in pyproject
    
    * update cellfinder to cellfinder_core
    
    * renaming cellfinder script workflow to cellfinder_core. fix entrypoint test.
    
    * docstrings for class config
    
    * fix entry point
    
    * skip add input paths test
    
    * feedback from code review

--- tests/cellfinder_core/test_unit/test_cellfinder.py
+++ tests/cellfinder_core/test_unit/test_cellfinder.py
@@ -6,43 +6,74 @@ from pathlib import Path
 import pooch
 import pytest
 
-from brainglobe_workflows.cellfinder_core.cellfinder import (
-    CellfinderConfig,
-    add_signal_and_background_files,
-    read_cellfinder_config,
-    run_workflow_from_cellfinder_run,
-    setup_workflow,
-)
-from brainglobe_workflows.cellfinder_core.cellfinder import setup as setup_full
 from brainglobe_workflows.utils import setup_logger
 
 
-@pytest.fixture()
-def default_input_config_cellfinder() -> Path:
-    """Return path to default input config for cellfinder workflow
+@pytest.fixture(scope="session")
+def cellfinder_GIN_data() -> dict:
+    """Return the URL and hash to the GIN repository with the input data
 
     Returns
     -------
-    Path
-        Path to default input config
-
+    dict
+        URL and hash of the GIN repository with the cellfinder test data
     """
-    from brainglobe_workflows.utils import DEFAULT_JSON_CONFIG_PATH_CELLFINDER
+    return {
+        "url": "https://gin.g-node.org/BrainGlobe/test-data/raw/master/cellfinder/cellfinder-test-data.zip",
+        "hash": "b0ef53b1530e4fa3128fcc0a752d0751909eab129d701f384fc0ea5f138c5914",  # noqa
+    }
 
-    return DEFAULT_JSON_CONFIG_PATH_CELLFINDER
+
+@pytest.fixture()
+def config_local(cellfinder_GIN_data, default_input_config_cellfinder):
+    """ """
+
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        CellfinderConfig,
+    )
+
+    # read default config as dict
+    # as dict because some paths are computed derived from input_data_dir
+    with open(default_input_config_cellfinder) as cfg:
+        config_dict = json.load(cfg)
+
+    # modify config:
+    # - remove url
+    # - remove data hash
+    # - add input_data_dir
+    config_dict["data_url"] = None
+    config_dict["data_hash"] = None
+    config_dict["input_data_dir"] = Path.home() / "local_cellfinder_data"
+
+    # instantiate object
+    config = CellfinderConfig(**config_dict)
+
+    # fetch data from GIN and download locally to local location?
+    pooch.retrieve(
+        url=cellfinder_GIN_data["url"],
+        known_hash=cellfinder_GIN_data["hash"],
+        path=Path(config.input_data_dir).parent,  # path to download zip to
+        progressbar=True,
+        processor=pooch.Unzip(
+            extract_dir=Path(config.input_data_dir).stem
+            # path to unzipped dir, *relative*  to 'path'
+        ),
+    )
+    return config
 
 
 @pytest.mark.parametrize(
-    "input_config",
+    "input_config, message",
     [
-        "input_data_GIN.json",
-        "input_data_locally.json",
-        "input_data_missing_background.json",
-        "input_data_missing_signal.json",
-        "input_data_not_locally_or_GIN.json",
+        ("default_input_config_cellfinder", "Using default config file"),
     ],
 )
-def test_read_cellfinder_config(input_config: str, input_configs_dir: Path):
+def test_read_cellfinder_config(
+    input_config: str,
+    message: str,
+    caplog: pytest.LogCaptureFixture,
+    request: pytest.FixtureRequest,
+):
     """Test for reading a cellfinder config file
 
     Parameters
@@ -52,11 +83,17 @@ def test_read_cellfinder_config(input_config: str, input_configs_dir: Path):
     input_configs_dir : Path
         Test data directory path
     """
-    # path to config json file
-    input_config_path = input_configs_dir / input_config
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        read_cellfinder_config,
+    )
+
+    # instantiate custom logger
+    _ = setup_logger()
+
+    input_config_path = request.getfixturevalue(input_config)
 
     # read json as Cellfinder config
-    config = read_cellfinder_config(input_config_path)
+    config = read_cellfinder_config(input_config_path, log_on=True)
 
     # read json as dict
     with open(input_config_path) as cfg:
@@ -67,37 +104,50 @@ def test_read_cellfinder_config(input_config: str, input_configs_dir: Path):
         [ky in config.__dataclass_fields__.keys() for ky in config_dict.keys()]
     )
 
+    # check logs
+    assert message in caplog.text
 
+    # check all signal files exist
+    assert config._list_signal_files
+    assert all([Path(f).is_file() for f in config._list_signal_files])
+
+    # check all background files exist
+    assert config._list_background_files
+    assert all([Path(f).is_file() for f in config._list_background_files])
+
+    # check output directory exists
+    assert Path(config._output_path).resolve().is_dir()
+
+    # check output directory name has correct format
+    out = re.fullmatch(
+        str(config.output_dir_basename) + "\\d{8}_\\d{6}$",
+        Path(config._output_path).stem,
+    )
+    assert out is not None
+    assert out.group() is not None
+
+    # check output file path is as expected
+    assert (
+        Path(config._detected_cells_path)
+        == Path(config._output_path) / config.detected_cells_filename
+    )
+
+
+@pytest.mark.skip(reason="focus of PR62")
 @pytest.mark.parametrize(
     "input_config, message_pattern",
     [
         (
-            "input_data_GIN.json",
-            "Fetching input data from the provided GIN repository",
-        ),
-        (
-            "input_data_locally.json",
+            "config_local",
             "Fetching input data from the local directories",
-        ),
-        (
-            "input_data_missing_background.json",
-            "The directory .+ does not exist$",
-        ),
-        ("input_data_missing_signal.json", "The directory .+ does not exist$"),
-        (
-            "input_data_not_locally_or_GIN.json",
-            "Input data not found locally, and URL/hash to "
-            "GIN repository not provided",
         ),
     ],
 )
-def test_add_signal_and_background_files(
+def test_add_input_paths(
     caplog: pytest.LogCaptureFixture,
-    tmp_path: Path,
-    cellfinder_GIN_data: dict,
-    input_configs_dir: Path,
     input_config: str,
     message_pattern: str,
+    request: pytest.FixtureRequest,
 ):
     """Test signal and background files addition to the cellfinder config
 
@@ -105,8 +155,6 @@ def test_add_signal_and_background_files(
     ----------
     caplog : pytest.LogCaptureFixture
         Pytest fixture to capture the logs during testing
-    tmp_path : Path
-        Pytest fixture providing a temporary path for each test
     cellfinder_GIN_data : dict
         Dict holding the URL and hash of the cellfinder test data in GIN
     input_configs_dir : Path
@@ -116,53 +164,12 @@ def test_add_signal_and_background_files(
     message_pattern : str
         Expected pattern in the log
     """
-    # instantiate our custom logger
+
+    # instantiate custom logger
     _ = setup_logger()
 
     # read json as Cellfinder config
-    config = read_cellfinder_config(input_configs_dir / input_config)
-
-    # monkeypatch cellfinder config:
-    # set install_path to pytest temporary directory
-    config.install_path = tmp_path / config.install_path
-
-    # check lists of signal and background files are not defined
-    assert not (config.list_signal_files and config.list_background_files)
-
-    # build fullpaths to input data directories
-    config.signal_dir_path = str(
-        Path(config.install_path)
-        / config.data_dir_relative
-        / config.signal_subdir
-    )
-    config.background_dir_path = str(
-        Path(config.install_path)
-        / config.data_dir_relative
-        / config.background_subdir
-    )
-
-    # monkeypatch cellfinder config:
-    # if config is "local" or "signal/background missing":
-    # ensure signal and background data from GIN are downloaded locally
-    if input_config in [
-        "input_data_locally.json",
-        "input_data_missing_signal.json",
-        "input_data_missing_background.json",
-    ]:
-        # fetch data from GIN and download locally
-        pooch.retrieve(
-            url=cellfinder_GIN_data["url"],
-            known_hash=cellfinder_GIN_data["hash"],
-            path=config.install_path,  # path to download zip to
-            progressbar=True,
-            processor=pooch.Unzip(
-                extract_dir=config.data_dir_relative
-                # path to unzipped dir, *relative*  to 'path'
-            ),
-        )
-
-    # add signal and background files lists to config
-    add_signal_and_background_files(config)
+    _ = request.getfixturevalue(input_config)
 
     # check log messages
     assert len(caplog.messages) > 0
@@ -172,92 +179,9 @@ def test_add_signal_and_background_files(
 
 
 @pytest.mark.parametrize(
-    "input_config, message",
-    [
-        ("default_input_config_cellfinder", "Using default config file"),
-        ("input_config_fetch_GIN", "Input config read from"),
-    ],
-)
-def test_setup_workflow(
-    input_config: str,
-    message: str,
-    monkeypatch: pytest.MonkeyPatch,
-    tmp_path: Path,
-    caplog: pytest.LogCaptureFixture,
-    request: pytest.FixtureRequest,
-):
-    """Test setup steps for the cellfinder workflow, using the default config
-    and passing a specific config file.
-
-    These setup steps include:
-    - instantiating a CellfinderConfig object using the input json file,
-    - add the signal and background files to the config if these are not
-      defined,
-    - create a timestamped directory for the output of the workflow if
-      it doesn't exist and add its path to the config
-
-    Parameters
-    ----------
-    input_config : str
-        Name of input config json file
-    message : str
-        Expected log message
-    monkeypatch : pytest.MonkeyPatch
-        Pytest fixture to use monkeypatching utils
-    tmp_path : Path
-        Pytest fixture providing a temporary path for each test
-    caplog : pytest.LogCaptureFixture
-        Pytest fixture to capture the logs during testing
-    request : pytest.FixtureRequest
-        Pytest fixture to enable requesting fixtures by name
-    """
-
-    # setup logger
-    _ = setup_logger()
-
-    # monkeypatch to change current directory to
-    # pytest temporary directory
-    # (cellfinder cache directory is created in cwd)
-    monkeypatch.chdir(tmp_path)
-
-    # setup workflow
-    config = setup_workflow(request.getfixturevalue(input_config))
-
-    # check logs
-    assert message in caplog.text
-
-    # check all signal files exist
-    assert config.list_signal_files
-    assert all([Path(f).is_file() for f in config.list_signal_files])
-
-    # check all background files exist
-    assert config.list_background_files
-    assert all([Path(f).is_file() for f in config.list_background_files])
-
-    # check output directory exists
-    assert Path(config.output_path).resolve().is_dir()
-
-    # check output directory name has correct format
-    out = re.fullmatch(
-        str(config.output_path_basename_relative) + "\\d{8}_\\d{6}$",
-        Path(config.output_path).stem,
-    )
-    assert out is not None
-    assert out.group() is not None
-
-    # check output file path
-    assert (
-        Path(config.detected_cells_path)
-        == Path(config.output_path) / config.detected_cells_filename
-    )
-
-
-@pytest.mark.parametrize(
     "input_config",
     [
         "default_input_config_cellfinder",
-        "input_config_fetch_GIN",
-        "input_config_fetch_local",
     ],
 )
 def test_setup(
@@ -283,6 +207,13 @@ def test_setup(
     request : pytest.FixtureRequest
         Pytest fixture to enable requesting fixtures by name
     """
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        CellfinderConfig,
+    )
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        setup as setup_full,
+    )
+
     # Monkeypatch to change current directory to
     # pytest temporary directory
     # (cellfinder cache directory is created in cwd)
@@ -304,14 +235,10 @@ def test_setup(
     "input_config",
     [
         "default_input_config_cellfinder",
-        "input_config_fetch_GIN",
-        "input_config_fetch_local",
     ],
 )
 def test_run_workflow_from_cellfinder_run(
     input_config: str,
-    monkeypatch: pytest.MonkeyPatch,
-    tmp_path: Path,
     request: pytest.FixtureRequest,
 ):
     """Test running cellfinder workflow with default input config
@@ -328,10 +255,12 @@ def test_run_workflow_from_cellfinder_run(
     request : pytest.FixtureRequest
         Pytest fixture to enable requesting fixtures by name
     """
-    # monkeypatch to change current directory to
-    # pytest temporary directory
-    # (cellfinder cache directory is created in cwd)
-    monkeypatch.chdir(tmp_path)
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        run_workflow_from_cellfinder_run,
+    )
+    from brainglobe_workflows.cellfinder_core.cellfinder_core import (
+        setup as setup_full,
+    )
 
     # run setup
     cfg = setup_full(str(request.getfixturevalue(input_config)))
@@ -340,4 +269,4 @@ def test_run_workflow_from_cellfinder_run(
     run_workflow_from_cellfinder_run(cfg)
 
     # check output files are those expected?
-    assert Path(cfg.detected_cells_path).is_file()
+    assert Path(cfg._detected_cells_path).is_file()
