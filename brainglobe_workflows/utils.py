f5cfcdb27908567e1ce252caaecbda0d9067e2a8
sfmig <33267254+sfmig@users.noreply.github.com>
2024-04-30 16:31:12 +0100

    remove arg parser

--- brainglobe_workflows/utils.py
+++ brainglobe_workflows/utils.py
@@ -1,8 +1,6 @@
-import argparse
 import logging
 import sys
 from pathlib import Path
-from typing import List
 
 DEFAULT_JSON_CONFIGS_PATH = Path(__file__).resolve().parent / "configs"
 
@@ -38,66 +36,3 @@ def setup_logger() -> logging.Logger:
     logger.setLevel(logging.DEBUG)
     logger.addHandler(console_handler)
     return logger
-
-
-def config_parser(
-    argv: List[str],
-    default_config: str,
-) -> argparse.Namespace:
-    """Define argument parser for a workflow script.
-
-    The only CLI argument defined in the parser is
-    the input config file. The list of input arguments
-    `argv` can be an empty list.
-
-    Both the list of input arguments and the default config to use if
-    no config is specified must be passed as an input to this
-    function.
-
-    Parameters
-    ----------
-    argv_ : List[str]
-        _description_
-    default_config : str
-        _description_
-
-    Returns
-    -------
-    args : argparse.Namespace
-        command line input arguments parsed
-    """
-
-    # initialise argument parser
-    parser = argparse.ArgumentParser(
-        description=(
-            """
-            To launch the cellfinder workflow with default parameters, run:
-                `cellfinder-workflow`.
-            The default parameters are those specifed in brainglobe_workflows/
-            cellfinder_core/configs/cellfinder.json.
-
-
-            To launch the cellfinder workflow with a specific set of input
-            parameters, run:
-                `cellfinder-workflow --config path/to/config.json`,
-            where `path/to/input/config.json` is the json file with the
-            desired parameters.
-            """
-        ),
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-
-    # add arguments
-    parser.add_argument(
-        "-c",
-        "--config",
-        default=default_config,
-        type=str,
-        metavar="CONFIG",  # a name for usage messages
-        help="",
-    )
-
-    # build parser object
-    args = parser.parse_args(argv)
-
-    return args
