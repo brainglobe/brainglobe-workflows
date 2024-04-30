f0e2797b732072ee2994f61c5cd619f7b2c8be16
sfmig <33267254+sfmig@users.noreply.github.com>
2024-04-30 16:35:29 +0100

    remove arg parser test

--- tests/cellfinder_core/test_unit/test_utils.py
+++ tests/cellfinder_core/test_unit/test_utils.py
@@ -1,11 +1,6 @@
 import logging
-from typing import List
-
-import pytest
 
 from brainglobe_workflows.utils import (
-    DEFAULT_JSON_CONFIG_PATH_CELLFINDER,
-    config_parser,
     setup_logger,
 )
 
@@ -24,23 +19,3 @@ def test_setup_logger(custom_logger_name: str):
     assert logger.name == custom_logger_name
     assert logger.hasHandlers()
     assert logger.handlers[0].name == "console_handler"
-
-
-@pytest.mark.parametrize(
-    "list_input_args",
-    [[], ["--config", str(DEFAULT_JSON_CONFIG_PATH_CELLFINDER)]],
-)
-def test_config_parser(list_input_args: List[str]):
-    """Test parser for config argument
-
-    Parameters
-    ----------
-    list_input_args : List[str]
-        a list with the command-line input arguments
-    """
-    args = config_parser(
-        list_input_args,
-        str(DEFAULT_JSON_CONFIG_PATH_CELLFINDER),
-    )
-
-    assert args.config
