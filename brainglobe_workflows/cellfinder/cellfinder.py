4700a23ca0eaec55f81396654f665fe7f20dd11c
Matt Einhorn <matt@einhorn.dev>
2024-05-07 09:11:06 -0400

    Support single z-stack tif file for input (#88)
    * Support single z-stack tif file for input.
    
    * Apply @K-Meech's suggestions from code review
    
    Co-authored-by: Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alessandro Felder <alessandrofelder@users.noreply.github.com>
    Co-authored-by: Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>

--- brainglobe_workflows/cellfinder/cellfinder.py
+++ brainglobe_workflows/cellfinder/cellfinder.py
@@ -26,7 +26,7 @@ from typing import Optional, Union
 import pooch
 from brainglobe_utils.IO.cells import save_cells
 from cellfinder.core.main import main as cellfinder_run
-from cellfinder.core.tools.IO import read_with_dask
+from cellfinder.core.tools.IO import read_z_stack
 from cellfinder.core.train.train_yml import depth_type
 
 from brainglobe_workflows.utils import (
@@ -352,8 +352,8 @@ def run_workflow_from_cellfinder_run(cfg: CellfinderConfig):
 
     The steps are:
     1. Read the input signal and background data as two separate
-       Dask arrays.
-    2. Run the main cellfinder pipeline on the input Dask arrays,
+       Dask arrays (or in-memory numpy arrays if single file tiff stack).
+    2. Run the main cellfinder pipeline on the input arrays,
        with the parameters defined in the input configuration (cfg).
     3. Save the detected cells as an xml file to the location specified in
        the input configuration (cfg).
@@ -364,9 +364,9 @@ def run_workflow_from_cellfinder_run(cfg: CellfinderConfig):
         a class with the required setup methods and parameters for
         the cellfinder workflow
     """
-    # Read input data as Dask arrays
-    signal_array = read_with_dask(str(cfg._signal_dir_path))
-    background_array = read_with_dask(str(cfg._background_dir_path))
+    # Read input data as Dask or numpy arrays
+    signal_array = read_z_stack(str(cfg._signal_dir_path))
+    background_array = read_z_stack(str(cfg._background_dir_path))
 
     # Run main analysis using `cellfinder_run`
     detected_cells = cellfinder_run(
