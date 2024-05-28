b853c39d092e73a5e2153a14c340079ccbb94228
IgorTatarnikov <igor.tatarnikov@gmail.com>
2024-05-28 20:53:49 +0100

    Set the backend for torch testing

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+import torch.backends.mps
+import keras.src.backend.common.global_state
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_backend():
+    os.environ["KERAS_BACKEND"] = "torch"
+    yield
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_device_arm_macos_ci():
+    """
+    Ensure that the device is set to CPU when running on arm based macOS
+    GitHub runners. This is to avoid the following error:
+    https://discuss.pytorch.org/t/mps-back-end-out-of-memory-on-github-action/189773/5
+    """
+    if (
+        os.getenv("GITHUB_ACTIONS") == "true"
+        and torch.backends.mps.is_available()
+    ):
+        keras.src.backend.common.global_state.set_global_attribute(
+            "torch_device", "cpu"
+        )
